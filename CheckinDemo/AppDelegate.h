aa31c2d68664a653653555d1b5dee9a7063b7c15
wenan.lwa <wenan.lwa@alibaba-inc.com>
2017-03-29 16:37:21 +0800

    init

---
+++ CheckinDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckinDemo
+//
+//  Created by eidan on 2017/3/29.
+//  Copyright © 2017年 Amap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheckinDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckinDemo
+//
+//  Created by eidan on 2017/3/29.
+//  Copyright © 2017年 Amap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
